123bc2bb7957f30808ee6fe79f61144e59ac5a46
Michael Dardis <dardismm@gmail.com>
2018-08-16 11:16:14 -0400

    Added story text, and subview navigation for choice buttons

--- InteractiveStory/PageController.swift
+++ InteractiveStory/PageController.swift
@@ -12,6 +12,13 @@ class PageController: UIViewController {
 
     var page: Page?
     
+    // MARK: - User Interface Props / Views
+    
+    let artworkView = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .system) //system button
+    let secondChoiceButton = UIButton(type: .system)
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -23,11 +30,34 @@ class PageController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
         
+        view.backgroundColor = .white
+        
+        //because this prop is optional, we have to unwrap it
         if let page = page {
-            print(page.story.text)
+            artworkView.image = page.story.artwork
+            
+            let attributedString = NSMutableAttributedString(string: page.story.text)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.lineSpacing = 10
+            
+            attributedString.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+            
+            //for styled text, assign it to the label's attributed text, not just .text
+            storyLabel.attributedText = attributedString
+            
+            if let firstCoice = page.firstChoice {
+                firstChoiceButton.setTitle(firstCoice.title, for: .normal)
+                firstChoiceButton.addTarget(self, action: #selector(PageController.loadFirstChoice), for: .touchUpInside)
+            } else {
+                firstChoiceButton.addTarget(self, action: #selector(PageController.playAgain), for: .touchUpInside)
+                firstChoiceButton.setTitle("Play Again", for: .normal)
+            }
+            
+            if let secondChoice = page.secondChoice {
+                secondChoiceButton.setTitle(secondChoice.title, for: .normal)
+                secondChoiceButton.addTarget(self, action: #selector(PageController.loadSecondChoice), for: .touchUpInside)
+            }
         }
     }
 
@@ -35,8 +65,88 @@ class PageController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        view.addSubview(artworkView)
+        
+        //if you dont do this line, the parent view will add its own constraints to its subviews
+        artworkView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //adding constraints programmatically. This code is done for you in IB behind the scenes if you do it through storyboard
+        NSLayoutConstraint.activate([
+             artworkView.topAnchor.constraint(equalTo: view.topAnchor),
+             artworkView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+             artworkView.rightAnchor.constraint(equalTo: view.rightAnchor),
+             artworkView.leftAnchor.constraint(equalTo: view.leftAnchor)
+            
+        ])
+        
+        //Story text sub view-------------------------------------------
+        view.addSubview(storyLabel)
+        storyLabel.numberOfLines = 0
+        storyLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            storyLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            storyLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+            storyLabel.topAnchor.constraint(equalTo: view.centerYAnchor, constant: -48.0)
+        ])
+        
+        //Choice buttons sub views-----------------------------------------
+        view.addSubview(firstChoiceButton)
+        firstChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            firstChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            firstChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80.0)
+            ])
+        
+        view.addSubview(secondChoiceButton)
+        secondChoiceButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            secondChoiceButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            secondChoiceButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -32)
+            ])
+    }
+    
+    @objc func loadFirstChoice() {
+        if let page = page, let firstChoice = page.firstChoice {
+            let nextPage = firstChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    @objc func loadSecondChoice() {
+        if let page = page, let secondChoice = page.secondChoice {
+            let nextPage = secondChoice.page
+            let pageController = PageController(page: nextPage)
+            
+            navigationController?.pushViewController(pageController, animated: true)
+        }
+    }
+    
+    @objc func playAgain() {
+        navigationController?.popToRootViewController(animated: true)
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
